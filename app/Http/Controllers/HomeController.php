e6509170b71210b5c9a5e87cffa3ed1a64ae05e8
FaruqAlfa <alfafahmi172@gmail.com>
2023-05-26 07:40:06 +0700

    Dashboard Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        return view('index', ['user'=>$user]);
     }
 }
