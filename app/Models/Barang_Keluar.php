cad0c9c5f6ae90e45b6894c978a446c78613bcca
anhar <fndedek@gmailcom>
2023-05-30 15:55:16 +0700

    Membuat table relasi

---
+++ app/Models/Barang_Keluar.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Barang;
+use App\Models\Staff;
+
+class Barang_Keluar extends Model
+{
+    use HasFactory;
+
+    protected $table='barang_keluar';
+    public $timestamps= false;
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'staff_id',
+        'jumlah',
+        'harga',
+        'total',
+        'tanggal_keluar',
+    ];
+    
+    public function staff(){
+        return $this->belongsTo(Staff::class);
+    }
+
+    public function barang(){
+        return $this->belongsTo(Barang::class);
+    }
+}
