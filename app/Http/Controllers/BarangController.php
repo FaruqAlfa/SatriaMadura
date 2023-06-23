79982799cf24f4dfeb5900417a9a91d32ffca071
anhar <fndedek@gmailcom>
2023-06-23 19:49:20 +0700

    update view barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -6,10 +6,90 @@ use Illuminate\Http\Request;
 
 class BarangController extends Controller
 {
-    public function getBarang()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        $barang = Barang::all(); // Mengambil semua data dari tabel menggunakan model
+        $barang = Barang::all();
+        return view('perbarangan.barang', ['Barang' => $barang]);
+    }
 
-        return view('perbarangan.barang', ['barang' => $barang]); // Mengembalikan data ke view dengan nama 'your-view'
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('perbarangan.CreateBarang');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_barang' =>'required',
+            'stok' =>'required',
+            'harga' =>'required',
+        ]);
+
+        Barang::create($request->all());
+
+        return redirect()->route('dashboardAdmin')->with('success', 'Barang Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Barang::find($id)->delete();
+        return redirect()->route('barang.index')->with('success', 'Berhasil dihapus');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -87,6 +87,9 @@ Route::middleware('auth:web')->group(function () {
 Route::middleware('auth:staff')->group(function (){
     Route::resource('/dashboard', StaffController::class);
     Route::get('/dashboardStaff', [StaffController::class, 'index'])->name('dashboardStaff');
+    Route::resource('/barangan', BarangController::class);
+    Route::get('/barang',[BarangController::class, 'index'])->name('barang');
+    Route::get('/inputbarang',[BarangController::class, 'create'])->name('inputbarang');
     Route::resource('/barangm', BarangMasukController::class);
     Route::get('/barangmasuk',[BarangMasukController::class, 'index'])->name('barangmasuk');
     Route::resource('/barangk', BarangKeluarController::class);
